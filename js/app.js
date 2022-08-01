bd763964ebd7315314bd2b41d988713a43a55b38
Amy Pierce <diablokitty@gmail.com>
2022-08-01 16:13:27 -0700

    figure out git stuff

--- js/app.js
+++ js/app.js
@@ -45,16 +45,21 @@ let clicks = 0;
 let clickAllowed = 25;
 let render = true;
 let displayObjects = [];
-//let voteRecord = [];
 let productObjectList = [];
 
+
+//connect images to the dom
 
 let image1 = document.querySelector('section img:first-child');
 let image2 = document.querySelector('section img:nth-child(2)');
 let image3 = document.querySelector('section img:nth-child(3)');
 
+
+//call the function for creating the image objects
 buildCurrentProducts();
 
+
+//constructor for the image objects
 function Product(name, fileExtension = 'jpg', views = 0, clicks = 0) {
   this.name = name;
   this.fileExtension = fileExtension;
@@ -63,12 +68,14 @@ function Product(name, fileExtension = 'jpg', views = 0, clicks = 0) {
   this.clicks = clicks;
 }
 
+//helper function for instantiating the object instances
 function makeProduct(name, fileExtension, views, clicks) {
 
   let newProduct = new Product(name, fileExtension, views, clicks);
   productObjectList.push(newProduct);
 }
 
+//checks for information stored in local memory and translates that to image objects
 function buildCurrentProducts() {
   ////check to see if there are products in memory
   let potentialVotes = localStorage.getItem('products');
@@ -89,7 +96,7 @@ function buildCurrentProducts() {
     }
   }
   else {
-
+    //build array of objects if there's nothing in local memory
     let productList = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'tauntaun', 'unicorn', 'water-can', 'wine-glass'];
     for (let i = 0; i < (productList.length); i++) {
 
@@ -100,12 +107,15 @@ function buildCurrentProducts() {
   }
 
 }
+
+//randomizes products that are shown by choosing numbers and then showing the object in that array index
 function getRandomProduct() {
 
   return Math.floor(Math.random() * productObjectList.length);
 }
 console.log(productObjectList);
 
+//builds intial array to ensure that the first three products are not repeats at each showing by checking the randomly generated numbers
 function renderProductList() {
 
   let displayProduct1 = getRandomProduct();
@@ -121,8 +131,8 @@ function renderProductList() {
   [displayProduct1, displayProduct2, displayProduct3];
   buildRandomArray();
 
-  //refactor this code to display the number of objects chosen by a user form
-
+  //TODO: (stretch goal) refactor this code to display the number of objects chosen by a user form
+  //shows images chosen by randomizer
   image1.src = productObjectList[displayObjects[0]].src;
   image1.alt = productObjectList[displayObjects[0]].name;
   productObjectList[displayObjects[0]].views++;
@@ -137,7 +147,7 @@ function renderProductList() {
   displayObjects.shift();
 
 }
-
+// expands array of random products that ensures that no product will be shown more than once in three iterations
 function buildRandomArray() {
   while (displayObjects.length < 7) {
     let displayProduct = getRandomProduct();
@@ -205,13 +215,30 @@ function renderResults() {
 productContainer.addEventListener('click', handleProductVoteClick);
 
 
-
-
-
 //chart stuff
 
-// const myChart = new Chart(
-//   document.getElementById('myChart'),
-//   config
-// );
+
+const data = {
+  labels:  ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'tauntaun', 'unicorn', 'water-can', 'wine-glass'],
+  datasets: [{
+    label: 'Views',
+    data: [300, 50, 100],
+    backgroundColor: [
+      'rgb(255, 99, 132)',
+      'rgb(54, 162, 235)',
+      'rgb(255, 205, 86)'
+    ],
+    hoverOffset: 4
+  }]
+};
+
+const myChart = new Chart(
+  document.getElementById('myChart').getContext('2d'),
+  config
+);
+
+const config = {
+  type: 'pie',
+  data: data,
+};
 
