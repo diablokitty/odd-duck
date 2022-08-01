31fac72f8181bca8aed82ea34d353601dc955d26
Amy Pierce <diablokitty@gmail.com>
2022-08-01 16:26:37 -0700

    correct js code

--- js/app.js
+++ js/app.js
@@ -5,20 +5,7 @@
 //I also had help from a tutor, so there is some logic that he suggested would handle things more efficiently.
 //We did discuss things extensively when that occurred, so I would understand it.
 //And I ported code from cookie-sales for a couple of things.
-
-
-/*Remaining Assignment Goals for Lab 12
-
-
-Add chart
-
-Stretch Goals
-Handle the display and voting for an arbitrary number of images
-Using a variable, declare in your JS how many images to show.
-Based on that value, dynamically create that many <img> tags
-Also based on that value, ensure that your randomizer is properly handling the specified number of images for display and repeat tracking.
-*/
-
+//I also got assistance from Brooke on the localStorage problem
 
 
 //construct object for images:
@@ -32,76 +19,72 @@ function handleSubmit(event) {
   form.reset();
 }
 
-
 form.addEventListener('submit', handleSubmit);
 
 
 //connect this code to the DOM
 let productContainer = document.querySelector('section');
 let voteButton = document.querySelector('section + div');
-
 let clicks = 0;
+//let productsShown = 3;
 let clickAllowed = 25;
 let render = true;
 let displayObjects = [];
-let voteRecord = [];
+//let voteRecord = [];
+let productObjectList = [];
 
-//let productNumber = 3;
-//make
 
 let image1 = document.querySelector('section img:first-child');
 let image2 = document.querySelector('section img:nth-child(2)');
 let image3 = document.querySelector('section img:nth-child(3)');
 
+buildCurrentProducts();
 
-function Product(name, fileExtension = 'jpg') {
+function Product(name, fileExtension = 'jpg', views = 0, clicks = 0) {
   this.name = name;
+  this.fileExtension = fileExtension;
   this.src = `images/${this.name}.${fileExtension}`;
-  this.views = 0;
-  this.clicks = 0;
+  this.views = views;
+  this.clicks = clicks;
 }
 
+function makeProduct(name, fileExtension, views, clicks) {
 
-useStoredProducts(name, src, views, clicks){
-//This will not work without changes to  the constructor
-  let Product = new product(name, };
-    
-    let productObjectList = [];
-    // Render and LI with information about the ordered drink:
-    drinkObj.renderADrink();
+  let newProduct = new Product(name, fileExtension, views, clicks);
+  productObjectList.push(newProduct);
 }
 
-let productList = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep.png', 'tauntaun', 'unicorn', 'water-can', 'wine-glass'];
-//NEED TO WRAP THIS LIST IN A '!CONTAINS" conditional statement so there aren't duplicate objects 
-
-function getStoredVotes() {
-  // check if I have drinks (order) in storage
-  let potentialVotes = localStorage.getItem('products')
-  // if there are no orders potentialOrders will be null
-  if (potentialOrders) {
+function buildCurrentProducts() {
+  ////check to see if there are products in memory
+  let potentialVotes = localStorage.getItem('products');
+  if (potentialVotes) {
     // turn the potential orders back into Plain old JavaScript objects
     let parsedVotes = JSON.parse(potentialVotes);
-    // turn them back into instances of Drinks
+    // turn them back into instances of products
     // run the data back througgh the constructor again - REINSTANTIATE
 
-    for (let order of parsedVotes) {
-      console.log(order);
+    for (let product of parsedVotes) {
+      console.log(product);
       // extract values from the POJOs
       let name = product.name;
-      let src = product.src;
-      let click = product.click;
-      let view= product.view;
+      let fileExtension = product.fileExtension;
+      let savedViews = product.views;
+      let savedClicks = product.clicks;
+      makeProduct(name, fileExtension, savedViews, savedClicks);
+    }
+  }
+  else {
 
-      // pass those values into my helper function that will create drink objects
-      makeADrink(name, drinkType, milk, size)
+    let productList = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'tauntaun', 'unicorn', 'water-can', 'wine-glass'];
+    for (let i = 0; i < (productList.length); i++) {
 
+      let product = new Product(productList[i]);
+      productObjectList.push(product);
+      console.log(productObjectList);
+    }
+  }
 
-for (let i = 0; i < (productList.length); i++) {
-  
-  let product = new Product(productList[i]);
-  productObjectList.push(product);
 }
-
 function getRandomProduct() {
 
   return Math.floor(Math.random() * productObjectList.length);
@@ -110,17 +93,17 @@ console.log(productObjectList);
 
 function renderProductList() {
 
-  // let displayProduct1 = getRandomProduct();
-  // let displayProduct2 = getRandomProduct();
-  // while(displayProduct2 === displayProduct1){
-  //   displayProduct2 = getRandomProduct();
-  // }
-  // let displayProduct3 = getRandomProduct();
-  // while(displayProduct3 === displayProduct1 || displayProduct3 === displayProduct2){
-  //   displayProduct3 = getRandomProduct();
-  // }
+  let displayProduct1 = getRandomProduct();
+  let displayProduct2 = getRandomProduct();
+  while (displayProduct2 === displayProduct1) {
+    displayProduct2 = getRandomProduct();
+  }
+  let displayProduct3 = getRandomProduct();
+  while (displayProduct3 === displayProduct1 || displayProduct3 === displayProduct2) {
+    displayProduct3 = getRandomProduct();
+  }
 
-  //  [displayProduct1,displayProduct2,displayProduct3];
+  [displayProduct1, displayProduct2, displayProduct3];
   buildRandomArray();
 
   //refactor this code to display the number of objects chosen by a user form
@@ -176,8 +159,7 @@ function storeVotes() {
   console.log(productObjectList);
   let stringifiedProducts = JSON.stringify(productObjectList);
   console.log(stringifiedProducts);
-  localStorage.setItem('products', voteRecord);
-
+  localStorage.setItem('products', stringifiedProducts);
 
   renderProductList();
   if (clicks === clickAllowed) {
@@ -193,8 +175,6 @@ function handleVoteButtonClick() {
 
 function renderResults() {
 
-  // for each  goat in my array, generate a LI
-  // ex: name had X views and was clicked on X times
   if (render) {
     for (let i = 0; i < productObjectList.length; i++) {
       let ul = document.querySelector('#results');
@@ -219,5 +199,3 @@ productContainer.addEventListener('click', handleProductVoteClick);
 //   document.getElementById('myChart'),
 //   config
 // );
-
-
